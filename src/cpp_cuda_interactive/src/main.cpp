ab1f665aa3f4ec29fbc9580788f7d2d767195aae
handabao <18862963156@163.com>
2026-04-12 19:48:03 +0800

    cpp 与 cu 混合编程

---
+++ src/cpp_cuda_interactive/src/main.cpp
@@ -0,0 +1,46 @@
+﻿#include "print_index.hpp"
+#include <iostream>
+
+void print_one_dim(int inputSize, int blockSize)
+{
+    int gridSize = inputSize / blockSize;
+
+    dim3 block(blockSize);
+    dim3 grid(gridSize);
+
+    // print_idx_device(block, grid);
+    // print_dim_device(block, grid);
+    // print_thread_idx_per_block_device(block, grid);
+    print_thread_idx_device(block, grid);
+}
+
+void print_two_dim(int inputSize, int blockSize)
+{
+    int gridSize = inputSize / blockSize;
+
+    dim3 block(blockSize, blockSize);
+    dim3 grid(gridSize, gridSize);
+
+    // print_idx_device(block, grid);
+    // print_dim_device(block, grid);
+    // print_thread_idx_per_block_device(block, grid);
+    print_thread_idx_device(block, grid);
+}
+
+int main(int argc, char *argv[])
+{
+    int inputSize;
+    int blockSize;
+
+    /// one-dimention test
+    // inputSize = 32;
+    // blockSize = 4;
+    // print_one_dim(inputSize, blockSize);
+
+    ///  two-dimention test
+    inputSize = 8;
+    blockSize = 4;
+    print_two_dim(inputSize, blockSize);
+
+    return 0;
+}
